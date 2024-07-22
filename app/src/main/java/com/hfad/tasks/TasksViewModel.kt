0aaa37cd5f106b3a6e3cd81fe0bdd25b9c201ce4
FayzullinFF <1fightr1@rambler.ru>
2024-07-22 17:56:12 +0500

    EditTaskFragment with taskId

--- app/src/main/java/com/hfad/tasks/TasksViewModel.kt
+++ app/src/main/java/com/hfad/tasks/TasksViewModel.kt
@@ -1,5 +1,7 @@
 package com.hfad.tasks
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.map
 import androidx.lifecycle.viewModelScope
@@ -7,11 +9,10 @@ import kotlinx.coroutines.launch
 
 class TasksViewModel(val dao: TaskDao) : ViewModel() {
     var newTaskName = ""
-
-    private val tasks = dao.getAll()
-    val tasksString = tasks.map() {
-        tasks -> formatTasks(tasks)
-    }
+    val tasks = dao.getAll()
+    private val _navigateToTask = MutableLiveData<Long?>()
+    val navigateToTask: LiveData<Long?>
+        get() = _navigateToTask
 
     fun addTask() {
         viewModelScope.launch {
@@ -21,16 +22,11 @@ class TasksViewModel(val dao: TaskDao) : ViewModel() {
         }
     }
 
-    fun formatTasks(tasks: List<Task>): String {
-        return tasks.fold("") {
-            str, item -> str + '\n' + formatTask(item)
-        }
+    fun onTaskClicked(taskId: Long) {
+        _navigateToTask.value = taskId
     }
 
-    fun formatTask(task: Task): String {
-        var str = "ID: ${task.taskId}"
-        str += '\n' + "Name: ${task.taskName}"
-        str += '\n' + "Complete: ${task.taskDone}" + '\n'
-        return str
+    fun onTaskNavigated() {
+        _navigateToTask.value = null
     }
 }
